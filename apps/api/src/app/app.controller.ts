3f42fb7a0ca5812a2faa1c3af957a2dbb78b1ab8
Carlos Lopez <clopez@applaudostudios.com>
2021-04-16 17:58:32 -0600

    add test requests

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 
-import { AppService } from './app.service';
+import { AppService, BlogEntry } from './app.service';
 
-@Controller()
+@Controller('entries')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post()
+  create(@Body() body: BlogEntry) {
+    return this.appService.create(body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.appService.delete(id);
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
+export interface BlogEntry {
+  title: string;
+  body: string;
+  timestamp?: Date;
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  entries: BlogEntry[] = [
+    {
+      title: 'Example Title',
+      body: 'Example Blog Entry',
+      timestamp: new Date(),
+    },
+  ];
+
+  getData(): BlogEntry[] {
+    return this.entries;
+  }
+
+  create(entry: BlogEntry) {
+    const { title, body } = entry;
+    const newEntry = {
+      title,
+      body,
+      timestamp: new Date(),
+    };
+
+    this.entries = [...this.entries, newEntry];
+  }
+
+  delete(id: number) {
+    this.entries = this.entries.filter((_, idx) => idx !== id);
   }
 }
