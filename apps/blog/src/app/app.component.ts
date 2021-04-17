ffd92e4a20107a5fdf256746217657b8f3f4ab72
Carlos Lopez <clopez@applaudostudios.com>
2021-04-16 18:27:19 -0600

    add example UI

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogEntry, DataService } from './services/data.service';
 
 @Component({
   selector: 'blog-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'blog';
+export class AppComponent implements OnInit {
+  entries: BlogEntry[];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch(): void {
+    this.dataService
+      .fetch()
+      .subscribe((response: BlogEntry[]) => (this.entries = response));
+  }
+
+  onSaveEntry(titleInput: HTMLInputElement, bodyInput: HTMLInputElement): void {
+    const entry = {
+      title: titleInput.value,
+      body: bodyInput.value,
+    };
+
+    this.dataService.save(entry).subscribe(() => {
+      this.fetch();
+      titleInput.value = '';
+      bodyInput.value = '';
+    });
+  }
+
+  onDeleteEntry(index: number): void {
+    this.dataService.delete(index).subscribe(() => this.fetch());
+  }
 }
---
+++ apps/blog/src/app/services/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface BlogEntry {
+  title: string;
+  body: string;
+  timestamp?: Date;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  fetch() {
+    return this.http.get<BlogEntry[]>('/api/entries');
+  }
+
+  save(entry: BlogEntry) {
+    return this.http.post('/api/entries', entry);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`/api/entries/${id}`);
+  }
+}
